efa5e51f6919fa5d05d937e7555c9567c3e9519d
Javier Valero Cejudo <javalce29@gmail.com>
2024-04-18 14:18:05 +0200

    Fix hydration error

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -10,48 +10,41 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { cn } from '@/lib/utils';
+import { useEffect, useState } from 'react';
+import { Skeleton } from './ui/skeleton';
 
 const THEMES = {
-  light: 'light',
-  dark: 'dark',
-  system: 'system',
-};
+  LIGHT: 'light',
+  DARK: 'dark',
+  SYSTEM: 'system',
+} as const;
 
 export function ThemeToggle() {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
 
-  const updateTheme = (theme: string) => () => setTheme(theme);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <Skeleton className='size-9 rounded-md' />;
+  }
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button size='icon' variant='ghost'>
           <span className='sr-only'>Elige el tema</span>
-          <SunIcon
-            className={cn('size-5', {
-              'scale-0': theme !== THEMES.light,
-              'scale-100': theme === THEMES.light,
-            })}
-          />
-          <MoonIcon
-            className={cn('absolute size-5', {
-              'scale-0': theme !== THEMES.dark,
-              'scale-100': theme === THEMES.dark,
-            })}
-          />
-          <DesktopIcon
-            className={cn('absolute size-5', {
-              'scale-0': theme !== THEMES.system,
-              'scale-100': theme === THEMES.system,
-            })}
-          />
+          {theme === THEMES.LIGHT && <SunIcon className='size-5' />}
+          {theme === THEMES.DARK && <MoonIcon className='size-5' />}
+          {theme === THEMES.SYSTEM && <DesktopIcon className='size-5' />}
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align='end'>
-        <DropdownMenuItem onClick={updateTheme(THEMES.light)}>Claro</DropdownMenuItem>
-        <DropdownMenuItem onClick={updateTheme(THEMES.dark)}>Oscuro</DropdownMenuItem>
-        <DropdownMenuItem onClick={updateTheme(THEMES.system)}>Sistema</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme(THEMES.LIGHT)}>Claro</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme(THEMES.DARK)}>Oscuro</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme(THEMES.SYSTEM)}>Sistema</DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
