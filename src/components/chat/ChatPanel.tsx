e569563125d15d12577ae378d1db92f0bed9293a
Javier Valero Cejudo <javalce29@gmail.com>
2024-05-06 11:31:28 +0200

    refactor: Update ChatPanel component to handle messages and input

--- src/components/chat/ChatPanel.tsx
+++ src/components/chat/ChatPanel.tsx
@@ -1,13 +1,32 @@
-import { useTranslations } from 'next-intl';
+import { type Message, type UseChatHelpers } from 'ai/react';
 import { ChatInput } from './ChatInput';
 
-export function ChatPanel() {
-  const t = useTranslations('chat');
-
+export function ChatPanel({
+  inputPlaceholder,
+  messages,
+  input,
+  handleSubmit,
+  handleInputChange,
+}: {
+  inputPlaceholder: string;
+  messages: Message[];
+} & Pick<UseChatHelpers, 'input' | 'handleInputChange' | 'handleSubmit'>) {
   return (
     <div className='fixed inset-x-0 bottom-0 w-full'>
+      {messages.length > 0 ? (
+        <ul>
+          {messages.map((message) => (
+            <li key={message.id}>{message.content}</li>
+          ))}
+        </ul>
+      ) : null}
       <div className='mx-auto sm:max-w-2xl sm:p-4'>
-        <ChatInput placeholder={t('input.placeholder')} />
+        <ChatInput
+          handleInputChange={handleInputChange}
+          handleSubmit={handleSubmit}
+          input={input}
+          placeholder={inputPlaceholder}
+        />
       </div>
     </div>
   );
