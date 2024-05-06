ad727d9c4c9a9b5a6321098de48945bc25bdb97f
Javier Valero Cejudo <javalce29@gmail.com>
2024-05-06 11:35:48 +0200

    refactor: Update ChatInput component to use useRef for form submission

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -1,64 +1,48 @@
 'use client';
 
-import { z } from 'zod';
-
 import { Button } from '@/components/ui/button';
-import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
-import { zodResolver } from '@hookform/resolvers/zod';
 import { PaperPlaneIcon } from '@radix-ui/react-icons';
-import { useForm } from 'react-hook-form';
+import { type UseChatHelpers } from 'ai/react';
+import { useRef } from 'react';
 import Textarea from 'react-textarea-autosize';
 
-const formSchema = z.object({
-  message: z.string().min(1),
-});
+export function ChatInput({
+  placeholder,
+  input,
+  handleInputChange,
+  handleSubmit,
+}: { placeholder: string } & Pick<UseChatHelpers, 'input' | 'handleInputChange' | 'handleSubmit'>) {
+  const form = useRef<HTMLFormElement>(null);
 
-type FormValues = z.infer<typeof formSchema>;
-
-export function ChatInput({ placeholder }: { placeholder: string }) {
-  const form = useForm<FormValues>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      message: '',
-    },
-  });
-
-  const onSubmit = (data: FormValues) => {
-    console.log(data);
+  const onKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter' && !e.shiftKey && !e.nativeEvent.isComposing) {
+      form.current?.requestSubmit();
+      e.preventDefault();
+    }
   };
 
   return (
-    <Form {...form}>
-      <form onSubmit={() => form.handleSubmit(onSubmit)}>
-        <div className='relative flex max-h-60 w-full grow flex-col justify-center overflow-hidden bg-background pr-10 sm:rounded-md sm:border sm:pr-12'>
-          <FormField
-            control={form.control}
-            name='message'
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Textarea
-                    autoComplete='off'
-                    autoCorrect='off'
-                    className='min-h-[60px] w-full resize-none overflow-hidden bg-background px-4 py-[1.3rem] focus-within:outline-none sm:text-sm'
-                    placeholder={placeholder}
-                    rows={1}
-                    spellCheck={false}
-                    tabIndex={0}
-                    {...field}
-                  />
-                </FormControl>
-              </FormItem>
-            )}
-          />
-          <div className='absolute bottom-4 right-2 sm:right-4'>
-            <Button disabled={!form.formState.isValid} size='icon' type='submit'>
-              <PaperPlaneIcon />
-              <span className='sr-only'>Enviar mensaje</span>
-            </Button>
-          </div>
+    <form ref={form} onSubmit={handleSubmit}>
+      <div className='relative flex max-h-60 w-full grow flex-col justify-center overflow-hidden bg-background pr-10 sm:rounded-md sm:border sm:pr-12'>
+        <Textarea
+          autoComplete='off'
+          autoCorrect='off'
+          className='min-h-[60px] w-full resize-none overflow-hidden bg-background px-4 py-[1.3rem] focus-within:outline-none sm:text-sm'
+          placeholder={placeholder}
+          rows={1}
+          spellCheck={false}
+          tabIndex={0}
+          value={input}
+          onChange={handleInputChange}
+          onKeyDown={onKeyDown}
+        />
+        <div className='absolute bottom-4 right-2 sm:right-4'>
+          <Button disabled={input === ''} size='icon' type='submit'>
+            <PaperPlaneIcon />
+            <span className='sr-only'>Enviar mensaje</span>
+          </Button>
         </div>
-      </form>
-    </Form>
+      </div>
+    </form>
   );
 }
